1ad69d8ef71fe47b641232eef0243051a9ae0ba1
elyfheim <boundlesselation@protonmail.com>
2026-08-02 11:32:31 +0700

    feat: setup prettierd

--- plugin/formatter.lua
+++ plugin/formatter.lua
@@ -2,6 +2,7 @@ vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		html = { "prettierd" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
